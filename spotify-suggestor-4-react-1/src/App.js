2e9c56a8b5111a102f03b1bc6797fe9effafaf37
Angelg10 <angelguzman97@gmail.com>
2020-05-27 13:57:14 -0400

    axios for recommended songs, and CRUD. map over song. not working with backend yet

--- spotify-suggestor-4-react-1/src/App.js
+++ spotify-suggestor-4-react-1/src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 // Components
 import Login from "./components/Login";
-import GettingStarted from "./components/GettingStarted";
+// import GettingStarted from "./components/GettingStarted";
 import SignUp from "./components/SignUp";
-import Home from "./components/Home";
 import About from "./components/About";
 import PrivateRoute from "./components/PrivateRoute";
 import Suggestor from "./components/Suggestor";
@@ -43,9 +42,9 @@ function App() {
           <About />
         </Route>
         {/* Home should probably take use to the suggestor??  and not sure we need as many Nav items considering*/}
-        <Route exact path='/'>
+        {/* <Route exact path='/'>
           <GettingStarted />
-        </Route>
+        </Route> */}
       </Switch>
     </div>
   );
